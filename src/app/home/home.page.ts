5ecfbcf2742ee6b150fac7a3ceb55d4f9e9e8108
Aeri <victoriacarlangelo@gmail.com>
2024-12-20 23:09:32 +0800

    Fetching events complete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; // Import OnInit interface
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit { // Implement OnInit
+  sections: any = [];
+  events: any = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  // Automatically call this method when the component is initialized
+  ngOnInit() {
+    this.getSections();
+    this.showAllEvents();
+  }
+
+  // Fetch sections from the API
+  getSections() {
+    this.http.get('http://localhost/acechedule/sections.php').subscribe((response) => {
+      console.log(response);
+      this.sections = response;
+    });
+  }
+
+ // Fetch all events for "Home"
+ showAllEvents() {
+  this.http.get('http://localhost/acechedule/events.php').subscribe((response) => {
+    console.log(response);
+    this.events = response;
+  });
+}
+  
+// Fetch events by a specific section
+showEventsBySection(sectionId: number) {
+  this.http.get(`http://localhost/acechedule/events.php?section_id=${sectionId}`).subscribe((response) => {
+    console.log(response);
+    this.events = response;
+  });
+}
 }
